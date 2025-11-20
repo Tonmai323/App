32f96da601b2bfcfe0b987e169e73e5144988948
Tonmai323 <pirat0653027802ton@gmail.com>
2025-11-20 09:48:32 +0700

    Create app.sql

---
+++ app.sql
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "Hell, World!";
+    echo "<br>";
+    echo "<h2>My Name is john Doe.</h2>";
+    ?>
+</body>
+</html>
